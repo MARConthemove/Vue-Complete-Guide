c413d295b4899e84c7238f07013571fa2dcb73e2
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-18 10:07:04 +0200

    Created same functionality with vue.js

--- 01 - Getting Started/app.js
+++ 01 - Getting Started/app.js
@@ -1,3 +1,21 @@
+// initializing vue.js
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: '',
+    }
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue)
+      this.enteredValue = ''
+    },
+  },
+}).mount('#app')
+
+// only javascript approach
+/*
 const buttonEl = document.querySelector('button')
 const inputEl = document.querySelector('input')
 const listEl = document.querySelector('ul')
@@ -11,3 +29,4 @@ function addGoal() {
 }
 
 buttonEl.addEventListener('click', addGoal)
+*/
