6bf2ca2d96580f7fcf48ed0b76eb1bf29c166597
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-18 13:02:04 +0200

    Created eventListeners with vue

--- 02 - Basics&Core Concepts - Dom Interaction/basics-03-events-starting-code/app.js
+++ 02 - Basics&Core Concepts - Dom Interaction/basics-03-events-starting-code/app.js
@@ -2,8 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-    };
+    }
   },
-});
+  methods: {
+    add: function () {
+      return (this.counter += 1)
+    },
+  },
+})
 
-app.mount('#events');
+app.mount('#events')
