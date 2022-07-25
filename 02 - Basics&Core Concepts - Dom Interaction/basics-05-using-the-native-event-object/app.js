8c065cef5ec613e11a70538f2652df17096cb0c7
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-25 10:05:58 +0200

    Small modification to Core concepts vue

--- 02 - Basics&Core Concepts - Dom Interaction/basics-05-using-the-native-event-object/app.js
+++ 02 - Basics&Core Concepts - Dom Interaction/basics-05-using-the-native-event-object/app.js
@@ -2,21 +2,21 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
-    };
+      name: '',
+    }
   },
   methods: {
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value + ' ' + lastName
     },
     add(num) {
-      this.counter = this.counter + num;
+      this.counter = this.counter + num
     },
     reduce(num) {
-      this.counter = this.counter - num;
+      this.counter = this.counter - num
       // this.counter--;
-    }
-  }
-});
+    },
+  },
+})
 
-app.mount('#events');
+app.mount('#events')
